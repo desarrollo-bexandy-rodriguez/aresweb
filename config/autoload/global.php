0f725ccccb439aaafd0778069d32d305d19640b0
Bexandy Rodríguez <bexandy@gmail.com>
2017-11-12 17:04:37 -0400

    Actualizacion de Noviembre

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -19,7 +19,6 @@ return array(
     ),
     'db' => array(
         'driver' => 'Pdo',
-        'dsn' => 'mysql:dbname=app_ares;host=localhost;charset=utf8',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
